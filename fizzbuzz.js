0e5f08440529b3cc08c3b3359eb3715a44c7d0e7
jaye3 <105278798+jaye3@users.noreply.github.com>
2024-04-29 10:50:30 +0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,17 @@
 function fizzbuzz(num) {
-    if (num % 15 === 0) return 'FizzBuzz';
-    if (num % 3 === 0) return 'Fizz';
-    if (num % 5 === 0) return 'Buzz';
-    return num;
+    // Construct a key from the divisibility of num by 3 and 5
+    const key = `${num % 3 === 0}-${num % 5 === 0}`;
+
+    switch (key) {
+        case "true-true":
+            return 'FizzBuzz';
+        case "true-false":
+            return 'Fizz';
+        case "false-true":
+            return 'Buzz';
+        default:
+            return num;
+    }
 }
 
 module.exports = fizzbuzz;
