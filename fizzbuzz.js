4cdec3046280c322ccf4fa31db95007830716f21
jaye3 <105278798+jaye3@users.noreply.github.com>
2024-04-29 11:10:15 +0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,10 @@
 function fizzbuzz(num) {
     // Construct a key from the divisibility of num by 3 and 5
+    if (typeof num !== 'number' || isNaN(num)) {
+        return null;
+    }
+    
     const key = `${num % 3 === 0}-${num % 5 === 0}`;
-
     switch (key) {
         case "true-true":
             return 'FizzBuzz';
