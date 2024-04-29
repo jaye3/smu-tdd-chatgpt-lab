05838d2b25d39617150d0c690a429e1741d68ecb
jaye3 <105278798+jaye3@users.noreply.github.com>
2024-04-29 10:46:28 +0800

    Update fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+function fizzbuzz(num) {
+    if (num % 15 === 0) return 'FizzBuzz';
+    if (num % 3 === 0) return 'Fizz';
+    if (num % 5 === 0) return 'Buzz';
+    return num;
+}
+
+module.exports = fizzbuzz;
